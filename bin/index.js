47ccb7516b3c7662222d0ab89d506809f705f574
Oyinkansola Yahaya <yahayaoyinkansola@gmail.com>
2024-12-23 17:26:37 +0100

    Add logic to create tasks.json file and add todos

--- bin/index.js
+++ bin/index.js
@@ -1,1 +1,42 @@
 #!/usr/bin/env node
+
+import fs from "node:fs/promises";
+import { randomUUID } from "node:crypto";
+
+async function addTodo(todos = []) {
+  const newTodo = {
+    id: randomUUID(),
+    description: process.argv[3],
+    status: "todo",
+    createdAt: new Date().toLocaleString(),
+    updatedAt: new Date().toLocaleString(),
+  };
+
+  todos.push(newTodo);
+
+  await fs.writeFile("tasks.json", JSON.stringify(todos));
+  process.stdout.write(`Tasks successfully added (ID: ${newTodo.id})`);
+}
+
+async function setupTodoList() {
+  try {
+    const todos = JSON.parse(await fs.readFile("tasks.json", "utf-8"));
+    addTodo(todos);
+  } catch (error) {
+    // Check if the error returned is file not found, and create the file
+    if (error.code === "ENOENT") {
+      addTodo();
+    } else {
+      throw error;
+    }
+  }
+}
+
+switch (process.argv[2]) {
+  case "add":
+    setupTodoList();
+    break;
+
+  default:
+    console.log("Invalid command given");
+}
